649781d9c4fccb5074cdfc508e8f1662b5f4f4fa
SomeDude <SomeDude@NuBL7aaJ6Cn4fB7GXFb9Zfi8w1FhPyW3oKgU9TweZMw>
2010-08-22 14:13:14 +0000

    automated commit based on fms-src-0.3.56.zip SHA256: 21ba25cc24f33356a3e326cd9e2bfc2b9066371200a817aef68b13ae73097a65

--- include/threadwrapper/cancelablethread.h
+++ include/threadwrapper/cancelablethread.h
@@ -2,8 +2,10 @@
 #define _cancelablethread_
 
 #include "cancelablerunnable.h"
+#include "../global.h"
 
 #include <Poco/Thread.h>
+#include <Poco/Mutex.h>
 
 /*
 	Poco::Thread does not have a virtual destructor, so we can't inherit safely.
@@ -13,10 +15,11 @@ class CancelableThread
 public:
 	CancelableThread():m_runnable(0),m_thread(new Poco::Thread),m_wasjoined(false)								{}
 	CancelableThread(const std::string &name):m_runnable(0),m_thread(new Poco::Thread(name)),m_wasjoined(false)	{}
-	CancelableThread(CancelableRunnable *runnable):m_runnable(runnable),m_thread(new Poco::Thread),m_wasjoined(false)									{ if(m_thread && m_runnable) { m_thread->start(*runnable); } }
-	CancelableThread(CancelableRunnable *runnable, const std::string &name):m_runnable(runnable),m_thread(new Poco::Thread(name)),m_wasjoined(false)	{ if(m_thread && m_runnable) { m_thread->start(*runnable); } }
+	CancelableThread(CancelableRunnable *runnable):m_runnable(runnable),m_thread(new Poco::Thread),m_wasjoined(false)									{ if(m_thread && m_runnable) { Guard guard(m_threadmutex); m_thread->start(*runnable); } }
+	CancelableThread(CancelableRunnable *runnable, const std::string &name):m_runnable(runnable),m_thread(new Poco::Thread(name)),m_wasjoined(false)	{ if(m_thread && m_runnable) { Guard guard(m_threadmutex); m_thread->start(*runnable); } }
 	~CancelableThread()
 	{
+		Guard guard(m_threadmutex);
 		if(m_thread)
 		{
 			if(m_thread->isRunning() && IsCancelled()==false)
@@ -42,17 +45,18 @@ public:
 	}
 
 	// CancelableThread takes ownership of runnable and will destroy it in the destructor
-	void Start(CancelableRunnable *runnable)	{ m_runnable=runnable; m_thread->start(*runnable); }
+	void Start(CancelableRunnable *runnable)	{ Guard guard(m_threadmutex); m_runnable=runnable; m_thread->start(*runnable); }
 
-	void Cancel()				{ if(m_runnable) { m_runnable->Cancel(); } }
-	const bool IsCancelled()	{ return m_runnable ? m_runnable->IsCancelled() : false; }
+	void Cancel()				{ Guard guard(m_threadmutex); if(m_runnable) { m_runnable->Cancel(); } }
+	const bool IsCancelled()	{ Guard guard(m_threadmutex); return m_runnable ? m_runnable->IsCancelled() : false; }
 
 	// these methods implemented from Poco::Thread
-	void join()					{ if(m_thread) { m_thread->join(); m_wasjoined=true; } }
-	bool isRunning() const		{ if(m_thread) { return m_thread->isRunning(); } else { return false; } }
+	void join()					{ Guard guard(m_threadmutex); if(m_thread) { m_thread->join(); m_wasjoined=true; } }
+	bool isRunning()			{ Guard guard(m_threadmutex); if(m_thread) { return m_thread->isRunning(); } else { return false; } }
 
-	const int GetThreadID() const
+	const int GetThreadID()
 	{
+		Guard guard(m_threadmutex);
 		if(m_thread)
 		{
 #if defined(POCO_VERSION) && POCO_VERSION>=0x01030600
@@ -67,6 +71,7 @@ public:
 private:
 
 	Poco::Thread *m_thread;
+	Poco::FastMutex m_threadmutex;
 	CancelableRunnable *m_runnable;
 	bool m_wasjoined;				// We must call join on ALL Poco threads, running or not, before deletion.  Otherwise there is a potential TLS leak in POSIX Thread implementation.
 
